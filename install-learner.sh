35f5db09a20d39c5c55da3155a18e32557fd5adb
Keegan Bowen <keegan.bowen@gmail.com>
2016-02-12 21:38:44 -0700

    Update install-learner.sh

--- install-learner.sh
+++ install-learner.sh
@@ -22,16 +22,18 @@ fi
 echo "Daemonizing"
 if [ whoami == root ]; then
 sec.pl --input=/var/log/net.log --conf=/etc/learner.cfg --log=/var/tmp/learner/learner-child.log --debug=6 --detach
-sec.pl --input=/var/log/new-deny.log --conf=/etc/learner-watcher.cfg --log=/var/tmp/learner/learner-watcher.log --debug=6 --detach
+sec.pl --input=/var/log/new-inp.log --conf=/etc/learner-watcher.cfg --log=/var/tmp/learner/learner-watcher.log --debug=6 --detach
   echo "Daemonized learner-watcher with the pid of:";
   ps -ef | grep "log=/var/tmp/learner/learner-watcher.log" | grep -v grep | awk '{print $2}' | tee /var/run/hist-event-reaction.pid
   echo "Daemonized learner with the pid of:";
   ps -ef | grep "log=/var/tmp/learner/learner.log" | grep -v grep | awk '{print $2}' | tee /var/run/hist-event-reaction.pid
 else
 sudo sec.pl --input=/var/log/net.log --conf=/etc/learner.cfg --log=/var/tmp/learner/learner-child.log --debug=6 --detach
-sudo sec.pl --input=/var/log/new-deny.log --conf=/etc/learner-watcher.cfg --log=/var/tmp/learner/learner-watcher.log --debug=6 --detach
+sudo sec.pl --input=/var/log/new-inp.log --conf=/etc/learner-watcher.cfg --log=/var/tmp/learner/learner-watcher.log --debug=6 --detach
   echo "Daemonized learner-watcher with the pid of:";
   ps -ef | grep "log=/var/tmp/learner/learner-watcher.log" | grep -v grep | awk '{print $2}' | tee /var/run/hist-event-reaction.pid
   echo "Daemonized learner with the pid of:";
   ps -ef | grep "log=/var/tmp/learner/learner.log" | grep -v grep | awk '{print $2}' | tee /var/run/hist-event-reaction.pid
 fi
+
+~   
